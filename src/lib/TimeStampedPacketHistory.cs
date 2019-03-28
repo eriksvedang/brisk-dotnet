ce1bb6bedd59521f41a4d3a8ae7f8c4429681d4f
Peter Bjorklund <piot@hotmail.com>
2019-03-28 18:38:30 +0100

    fix: move TimeStampedPacketHistory from linger

---
+++ src/lib/TimeStampedPacketHistory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Piot.Log;
+using Piot.Tend.Client;
+
+namespace Piot.Linger
+{
+    public class PacketHistoryItem
+    {
+        public long Time;
+        public SequenceId SequenceId;
+    }
+
+    public class TimeStampedPacketHistory
+    {
+        Queue<PacketHistoryItem> items = new Queue<PacketHistoryItem>();
+        SequenceId lastSequenceId = SequenceId.Max;
+
+        public void SentPacket(SequenceId sequenceId, long time, ILog log)
+        {
+            //log.Info($"sent packet at timen {sequenceId} time {time}");
+            var p = new PacketHistoryItem { Time = time, SequenceId = sequenceId };
+            items.Enqueue(p);
+        }
+
+        public bool ReceivedConfirmation(SequenceId sequenceId, out PacketHistoryItem foundItem, ILog log)
+        {
+            //log.Info($"received confirmation {sequenceId} last {lastSequenceId}");
+            if (sequenceId.Value == lastSequenceId.Value)
+            {
+                foundItem = null;
+                return false;
+            }
+            while (true)
+            {
+                var item = items.Dequeue();
+                if (sequenceId.Value == item.SequenceId.Value)
+                {
+                    lastSequenceId = sequenceId;
+                    foundItem = item;
+                    return true;
+                }
+            }
+        }
+    }
+}
